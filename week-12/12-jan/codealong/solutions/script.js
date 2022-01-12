bb000e0049b5335930e9f3ffa117ee30b4bd8bfc
Dannie Håkansson <dannie.hakansson@cmeducations.se>
2022-01-12 15:25:29 +0100

    Added continued solution to codealong 12th of jan

---
+++ week-12/12-jan/codealong/solutions/script.js
@@ -0,0 +1,39 @@
+const select = document.querySelector("select");
+
+const confirmedElement = document.querySelector(".confirmed");
+const deathsElement = document.querySelector(".deaths");
+const recoveredElement = document.querySelector(".recovered");
+const criticalElement = document.querySelector(".critical");
+
+select.addEventListener("change", async (event) => {
+  const chosenCountry = event.target.value;
+  const url = `https://covid-19-data.p.rapidapi.com/country?name=${chosenCountry}`;
+
+  // Download relevant data based on country chosen!!!
+  const response = await fetch(url, {
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+      "x-rapidapi-key": "459ea5d62fmsh6411ec02bb9a765p138cacjsn81b007d6fc15",
+    },
+  });
+  const data = await response.json();
+
+  /* Object destructuring version (look inside the object for properties and create variables
+        with those same names) */
+  const { confirmed, deaths, recovered, critical } = data[0];
+
+  confirmedElement.textContent = confirmed;
+  deathsElement.textContent = deaths;
+  recoveredElement.textContent = recovered;
+  criticalElement.textContent = critical;
+
+  console.log(confirmed, deaths, recovered, critical);
+});
+
+// Google API key: AIzaSyDzn6x8l5LwxutxJxnlpjjYqCvPiCgOnPw
+
+// Initialize and add the map
+function initMap() {
+  console.log("Init map!!!");
+}
